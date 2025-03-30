86eb52a172604f22fc276004b6eb241b9e665206
zjh <155434453+1036690588@users.noreply.github.com>
2025-03-30 20:42:54 +0800

    定时任务，分布式锁以及Team接口

---
+++ src/main/java/com/yupi/usercenter/controller/TeamController.java
@@ -0,0 +1,130 @@
+package com.yupi.usercenter.controller;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.yupi.usercenter.common.BaseResponse;
+import com.yupi.usercenter.common.ErrorCode;
+import com.yupi.usercenter.common.ResultUtils;
+import com.yupi.usercenter.exception.BusinessException;
+import com.yupi.usercenter.model.domain.Team;
+import com.yupi.usercenter.model.domain.User;
+import com.yupi.usercenter.model.domain.request.TeamAddRequest;
+import com.yupi.usercenter.model.domain.request.UserLoginRequest;
+import com.yupi.usercenter.model.domain.request.UserRegisterRequest;
+import com.yupi.usercenter.model.vo.TeamQuery;
+import com.yupi.usercenter.service.TeamService;
+import com.yupi.usercenter.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.BeanUtils;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import static com.yupi.usercenter.contant.UserConstant.USER_LOGIN_STATE;
+
+/**
+ * 队伍接口
+ *
+ */
+@RestController
+@RequestMapping("/team")
+@CrossOrigin
+@Slf4j
+public class TeamController {
+
+    @Resource
+    private TeamService teamService;
+
+    @Resource
+    private UserService userService;
+
+    @Resource
+    private RedisTemplate<String,Object> redisTemplate;
+
+    @PostMapping("add")
+    public BaseResponse<Long> addTeam(@RequestBody TeamAddRequest teamAddRequest, HttpServletRequest request){
+        if(teamAddRequest == null){
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        User loginUser = userService.getLoginUser(request);
+        Team team = new Team();
+        BeanUtils.copyProperties(teamAddRequest,team);
+        Long result = teamService.addTeam(team, loginUser);
+        return ResultUtils.success(result);
+    }
+
+    @PostMapping("delete")
+    public BaseResponse<Boolean> deleteTeam(@RequestBody Long id){
+        if(id <= 0){
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        boolean remove = teamService.removeById(id);
+        if(!remove){
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR,"删除失败");
+        }
+        return ResultUtils.success(true);
+    }
+
+    @PostMapping("update")
+    public BaseResponse<Boolean> updateTeam(@RequestBody Team team){
+        if(team == null){
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        boolean update = teamService.updateById(team);
+        if(!update){
+            throw new BusinessException(ErrorCode.SYSTEM_ERROR,"更新失败");
+        }
+        return ResultUtils.success(true);
+    }
+
+    @GetMapping("get")
+    public BaseResponse<Team> getTeamById(Long id){
+        if(id <= 0){
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        Team team = teamService.getById(id);
+        if(team == null){
+            throw new BusinessException(ErrorCode.NULL_ERROR);
+        }
+        return ResultUtils.success(team);
+    }
+
+
+    @GetMapping("list")
+    public BaseResponse<List<Team>> list(TeamQuery teamQuery){
+        if(teamQuery == null){
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        Team team = new Team();
+        BeanUtils.copyProperties(teamQuery,team);
+        QueryWrapper<Team> teamQueryWrapper = new QueryWrapper<>();
+        List<Team> teamList = teamService.list(teamQueryWrapper);
+        return ResultUtils.success(teamList);
+    }
+
+    @GetMapping("list/page")
+    public BaseResponse<IPage<Team>> pageList(TeamQuery teamQuery){
+        if(teamQuery == null){
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        Team team = new Team();
+        BeanUtils.copyProperties(teamQuery,team);
+        QueryWrapper<Team> teamQueryWrapper = new QueryWrapper<>();
+        Page<Team> teamPage = new Page<>(teamQuery.getPageNum(), teamQuery.getPageSize());
+        IPage<Team> page = teamService.page(teamPage, teamQueryWrapper);
+        return ResultUtils.success(page);
+    }
+
+
+
+
+}
---
+++ src/main/java/com/yupi/usercenter/service/TeamService.java
@@ -0,0 +1,16 @@
+package com.yupi.usercenter.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.yupi.usercenter.model.domain.Team;
+import com.yupi.usercenter.model.domain.User;
+
+/**
+* @author admin
+* @description 针对表【team(队伍)】的数据库操作Service
+* @createDate 2025-03-29 10:36:08
+*/
+public interface TeamService extends IService<Team> {
+
+    public long addTeam(Team team, User loginUser);
+
+}
